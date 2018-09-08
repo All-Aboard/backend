c5dabb2d2ff7ade3d8856ccb96977eb5289d4050
Evangelos Pappas <epappas@evalonlabs.com>
2018-09-08 11:38:33 +0100

    web3 bootstrap

--- app.js
+++ app.js
@@ -3,18 +3,43 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const path = require('path');
 const glob = require('glob');
+const helmet = require('helmet');
 
 const app = express();
 
+const Web3 = require('web3');
+
+const web3 = new Web3();
+
 const BASE_ROUTE = '/v1';
 
+const HTTP_PROVIDER = 'http://0.0.0.0:8545';
+
+const accounts = [];
+const contracts = {};
+
+web3.setProvider(new web3.providers.HttpProvider(HTTP_PROVIDER));
+
 app.use(logger('dev'));
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({
     extended: false
 }));
 app.use(cookieParser());
 
+web3.eth.getAccounts().then((accList) => {
+    accList.forEach((acc, i) => accounts[i] = acc);
+    console.log('Accounts:', accounts);
+});
+
+app.use((req, res, next) => {
+    req.injections = {
+        web3, accounts,contracts
+    };
+
+    next();
+});
 
 [].concat(
     glob.sync('./**/*.js', {cwd: path.join(__dirname, 'routes')})
--- routes/bootstrap.js
+++ routes/bootstrap.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
+    const {web3} = req.injections;
+    const {account, ensName, sign} = req.body;
+
+    console.log('account, ensName, sign', account, ensName, sign);
+
     res.send({msg:'account created'});
 });
 
