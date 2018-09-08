ee3042c969f1a46470627babf3d810e1a5b3cb95
Evangelos Pappas <epappas@evalonlabs.com>
2018-09-08 11:10:15 +0100

    More route bootstrapping

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
+const path = require('path');
+const glob = require('glob');
 
 const app = express();
+
+const BASE_ROUTE = '/v1';
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,6 +15,15 @@ app.use(express.urlencoded({
 }));
 app.use(cookieParser());
 
-app.use('/v1', indexRouter);
+
+[].concat(
+    glob.sync('./**/*.js', {cwd: path.join(__dirname, 'routes')})
+).forEach((filename) => {
+    const route = filename.split('.').slice(0, -1).join('');
+
+    app.use(`${BASE_ROUTE}${route}`, require(path.join(__dirname, 'routes', filename)));
+});
+
+app.use(`${BASE_ROUTE}`, require(path.join(__dirname, 'routes', 'index.js')));
 
 module.exports = app;
