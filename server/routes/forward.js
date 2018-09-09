2346c6d058df75598ed762b90a658e19d4cf82f6
Evangelos Pappas <epappas@evalonlabs.com>
2018-09-09 06:29:06 +0100

    Fixing nounce

--- server/routes/forward.js
+++ server/routes/forward.js
@@ -6,31 +6,59 @@ const EthereumTx = require('ethereumjs-tx');
 const lightwallet = require('eth-lightwallet');
 const txutils = lightwallet.txutils;
 
-let Nonce = 20
-
 router.post('/', async (req, res, next) => {
     const {web3, contracts, addresses, MAIN_ADDR, privateKey} = req.injections;
     const {dataSign, toAddr, value} = req.body;
 
-    const txParams = {
-        nonce: (Nonce++),
-        gasLimit: web3.utils.toHex(5000000),
-        gasPrice: web3.utils.toHex(100000000000),
-        to: toAddr,
-        value: value || '0x00',
-        from: MAIN_ADDR,
-        data: dataSign
-    };
+    getNonce(MAIN_ADDR, async (err, txnsCount) => {
+        console.log('txnsCount', txnsCount);
 
-    const tx = new EthereumTx(txParams);
-    tx.sign(privateKey);
+        const txParams = {
+            nonce: txnsCount,
+            gasLimit: web3.utils.toHex(5000000),
+            gasPrice: web3.utils.toHex(100000000000),
+            to: toAddr,
+            value: value || '0x00',
+            from: MAIN_ADDR,
+            data: dataSign
+        };
 
-    console.log(tx.serialize().toString('hex'));
+        const tx = new EthereumTx(txParams);
+        tx.sign(privateKey);
 
-    web3.eth.sendSignedTransaction('0x' + tx.serialize().toString('hex')).on('receipt', console.log).catch( (e) => {
-        console.log(e)
+        console.log(tx.serialize().toString('hex'));
+
+        await web3.eth.sendSignedTransaction('0x' + tx.serialize().toString('hex')).on('receipt', console.log).catch( (e) => {
+            console.log(e)
+        });
+
         res.send({});
     });
 });
 
 module.exports = router;
+
+
+function getNonce(address, callback) {
+    web3.eth.getTransactionCount(address, (error, result) => {
+        var txnsCount = result;
+        web3.currentProvider.sendAsync({
+            method: "txpool_content",
+            params: [],
+            jsonrpc: "2.0",
+            id: new Date().getTime()
+        }, (error, result) => {
+            if (result.result.pending) {
+                if (result.result.pending[address]) {
+                    txnsCount = txnsCount +
+                        Object.keys(result.result.pending[address]).length;
+                    callback(null, txnsCount);
+                } else {
+                    callback(null, txnsCount);
+                }
+            } else {
+                callback(null, txnsCount);
+            }
+        })
+    })
+}
